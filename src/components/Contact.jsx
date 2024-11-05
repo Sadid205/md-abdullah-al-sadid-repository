1840d374b3778f866e991acf0a25e6595936a204
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2024-11-05 23:33:26 +0600

    added contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,9 @@
+import { FaWhatsapp } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Contact = ()=>{
     return (
         <div className="h-full w-11/12 m-auto flex flex-col gap-4 sm:ms-auto">
@@ -5,9 +11,24 @@ const Contact = ()=>{
                 <h1 className="text-gray-600 font-bold text-xl">CONTACT</h1>
                 <h1 className="text-gray-600 font-bold text-xl">06</h1>
             </div>
-            <div>
-                <form className="flex flex-col items-center">
-                    <div className="md:w-3/4 lg:w-2/3 xl:w-1/2">
+            <div className="sm:flex sm:w-11/12 m-auto">
+                <div className="sm:w-1/2">
+                    <h1 className="text-gray-400 font-bold">Name : MD.ABDULLAH AL SADID</h1>
+                    <h1 className="text-gray-400 font-bold">Address : Jhenaidah,Bangladesh</h1>
+                    <div className="flex items-center">
+                        <h1 className="text-gray-400 font-bold">Mobile : 01881543084</h1>
+                        <span className="text-gray-400 text-xl"><FaWhatsapp /></span>
+                    </div>
+                    <h1 className="text-gray-400 font-bold">Gmail : abdullahalsadid1914@gmail.com</h1>
+                    <div className="flex gap-4 mt-2">
+                        <Link to={"https://www.facebook.com/md.abdullahalsadid19/"} className="text-gray-400 text-xl"><FaFacebook /></Link>
+                        <Link to={"https://x.com/Sadid58827821"} className="text-gray-400 text-xl"><FaXTwitter /></Link>
+                        <Link to={"https://www.linkedin.com/in/abdullah-al-sadid/"} className="text-gray-400 text-xl"><FaLinkedin /></Link>
+                        <Link to={"https://github.com/Sadid205"} className="text-gray-400 text-xl"><FaGithub /></Link>
+                    </div>
+                </div>
+                <form className="flex sm:w-1/2 flex-col items-center">
+                    <div className="">
                     <div className="flex flex-col md:flex-row">
                         <input id="name" type="text" className="my-2 py-2 px-4 rounded-md bg-gray-800 text-gray-300 w-full md:w-1/2 md:mr-2 outline-none focus:ring-2 focus:ring-blue-600" placeholder="Name" />
                         <input id="email" type="email" className="my-2 py-2 px-4 rounded-md bg-gray-800 text-gray-300 w-full md:w-1/2 md:ml-2 outline-none focus:ring-2 focus:ring-blue-600" placeholder="Email" />
